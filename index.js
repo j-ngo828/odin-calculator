404c8b2ed0229c355523c979439db7854928404b
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-03-03 09:00:20 -0500

    Finish operate function

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ function multiply(op1, op2) {
 function divide(op1, op2) {
   return op1 / op2;
 }
+
+function operate(operator, op1, op2) {
+  switch (operator) {
+    case "+":
+      return add(op1, op2);
+    case "-":
+      return subtract(op1, op2);
+    case "*":
+      return multiply(op1, op2);
+    case "/":
+      return divide(op1, op2);
+  }
+}
