36684ccf8a433165258056724f967b82c08946d1
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-03-03 08:58:22 -0500

    Finish basic operation methods

---
+++ index.js
@@ -0,0 +1,21 @@
+const calculator = {
+  operand1: null,
+  operand2: null,
+  result: null,
+};
+
+function add(op1, op2) {
+  return op1 + op2;
+}
+
+function subtract(op1, op2) {
+  return op1 - op2;
+}
+
+function multiply(op1, op2) {
+  return op1 * op2;
+}
+
+function divide(op1, op2) {
+  return op1 / op2;
+}
